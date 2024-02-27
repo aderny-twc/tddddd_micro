8e061f41a50ea6f75e74442bbbecd04b4cd6b5b2
Andrey Belskiy <anb@smena.space>
2024-02-27 09:46:08 +0500

    add firs iteration of sync realization

---
+++ snippets/hash_file.py
@@ -0,0 +1,25 @@
+import hashlib
+from pathlib import Path
+
+BLOCKSIZE = 65536
+
+
+def hash_file(path: str | Path) -> str:
+    hasher = hashlib.sha1()
+
+    with path.open("rb") as file:
+        # Read block
+        buf = file.read(BLOCKSIZE)
+        # if file hasn't ended, update hash
+        while buf:
+            hasher.update(buf)
+            buf = file.read(BLOCKSIZE)
+        return hasher.hexdigest()
+
+
+if __name__ == '__main__':
+    path = Path(
+        '/home/ando/MEGAsync/playground/tddddd_micro/snippets/test_sync/source/file.txt'
+    )
+    result = hash_file(path)
+    print(result)
---
+++ snippets/sync.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+from pathlib import Path
+
+from hash_file import hash_file
+
+
+def sync(source: Path, dest: Path) -> None:
+    source_hashes = {}
+    for folder, _, files in os.walk(source):
+        for filename in files:
+            source_hashes[hash_file(Path(folder) / filename)] = filename
+
+    seen = set()
+
+    for folder, _, files in os.walk(dest):
+        for filename in files:
+            dest_path = Path(folder) / filename
+            dest_hash = hash_file(dest_path)
+            seen.add(dest_hash)
+
+            if dest_hash not in source_hashes:
+                # dest_path.resolve()
+                os.remove(dest_path)
+
+            elif dest_hash in source_hashes and filename != source_hashes[dest_hash]:
+                shutil.move(dest_path, Path(folder) / source_hashes[dest_hash])
+
+    for src_hash, filename in source_hashes.items():
+        if src_hash not in seen:
+            shutil.copy(Path(source) / filename, Path(dest) / filename)
+
+
+if __name__ == "__main__":
+    source = Path("/home/ando/MEGAsync/playground/tddddd_micro/snippets/test_sync/source/")
+    dest = Path("/home/ando/MEGAsync/playground/tddddd_micro/snippets/test_sync/dest/")
+
+    sync(source, dest)
