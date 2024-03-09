841a94b933297fa00d2ba2acfea2398c2f58f72b
Andrey Belskiy <anb@smena.space>
2024-03-09 09:28:54 +0500

    add test from domain in service layer

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,8 +1,13 @@
 from domain import model
 import pytest
 from service_layer import services
+from datetime import date, timedelta
 
 from adapters.repository import FakeRepository
+
+today = date.today()
+tomorrow = today + timedelta(days=1)
+later = tomorrow + timedelta(days=2)
 
 
 class FakeSession:
@@ -38,3 +43,16 @@ def test_commits():
 
     services.allocate(line, repo, session)
     assert session.committed is True
+
+
+def test_prefers_warehouse_batches_to_shipments():
+    in_stock_batch = model.Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+    shipment_batch = model.Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+    repo = FakeRepository([in_stock_batch, shipment_batch])
+    session = FakeSession()
+    line = model.OrderLine('oref', "RETRO-CLOCK", 10)
+
+    services.allocate(line, repo, session)
+
+    assert in_stock_batch.available_quantity == 90
+    assert shipment_batch.available_quantity == 100
