9995d7203c9f91a311259e92b0e8da7a90626d69
Andrey Belskiy <anb@smena.space>
2024-03-10 09:52:08 +0500

    Add endpoint add batch

--- entrypoints/main.py
+++ entrypoints/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from datetime import date
 
 import config
 from domain import model
@@ -20,7 +21,7 @@ class OrderLineModel(BaseModel):
     qty: int
 
 
-@app.post("/allocate/", status_code=201)
+@app.post("/allocate", status_code=201)
 def allocate_in_batch(order_line: OrderLineModel):
     session = get_session()
     repo = repository.SqlAlchemyRepository(session)
@@ -29,8 +30,27 @@ def allocate_in_batch(order_line: OrderLineModel):
         batchref = services.allocate(order_line.orderid, order_line.sku, order_line.qty, repo, session)
     except (model.OutOfStock, services.InvalidSku) as e:
         return JSONResponse(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                content={"message": str(e)}
-            )
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"message": str(e)}
+        )
 
     return {"batchref": batchref}
+
+
+class BatchModel(BaseModel):
+    ref: str
+    sku: str
+    qty: int = Field(gt=0)
+    eta: date | None = None
+
+
+@app.post("/batch", status_code=201)
+def add_batch(batch: BatchModel):
+    session = get_session()
+    repo = repository.SqlAlchemyRepository(session)
+
+    services.add_batch(
+        batch.ref, batch.sku, batch.qty, batch.eta, repo, session
+    )
+
+    return {"success": True}
