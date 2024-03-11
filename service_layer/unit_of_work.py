8698d06ea74543dafa1b0acdf1350128d8691cc8
Andrey Belskiy <anb@smena.space>
2024-03-11 09:37:29 +0500

    Add abstract uow

---
+++ service_layer/unit_of_work.py
@@ -0,0 +1,20 @@
+import abc
+from adapters import repository
+
+
+class AbstractUnitOfWork(abc.ABC):
+    batches: repository.AbstractRepository
+
+    def __enter__(self) -> "AbstractUnitOfWork":
+        return self
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    @abc.abstractmethod
+    def commit(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rollback(self):
+        raise NotImplementedError
