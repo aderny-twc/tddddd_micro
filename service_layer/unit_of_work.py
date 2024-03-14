6a8a8453ca71e276d1159c9bd729e280075e51fe
Andrey Belskiy <anb@smena.space>
2024-03-14 10:20:47 +0500

    Add real uow

--- service_layer/unit_of_work.py
+++ service_layer/unit_of_work.py
@@ -1,5 +1,12 @@
 import abc
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import config
 from adapters import repository
+
+DEFAULT_SESSION_FACTORY = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 
 
 class AbstractUnitOfWork(abc.ABC):
@@ -18,3 +25,23 @@ class AbstractUnitOfWork(abc.ABC):
     @abc.abstractmethod
     def rollback(self):
         raise NotImplementedError
+
+
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
+        self.session_factory = session_factory
+
+    def __enter__(self):
+        self.session = self.session_factory()
+        self.batches = repository.SqlAlchemyRepository(self.session)
+        return super().__enter__()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        super().__exit__(exc_type, exc_val, exc_tb)
+        self.session.close()
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
