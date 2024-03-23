0be3ebfc85d8ba165184a91f924ef489dd34894c
Andrey Belskiy <anb@smena.space>
2024-03-23 09:34:40 +0500

    Update tests

--- tests/integration/test_uow.py
+++ tests/integration/test_uow.py
@@ -4,42 +4,44 @@ from service_layer import unit_of_work
 from sqlalchemy.sql import text
 
 
-def insert_batch(session, ref, sku, qty, eta):
+def insert_batch(session, ref, sku, qty, eta, product_version=1):
+    session.execute(
+        text("INSERT INTO products (sku, version_number) VALUES (:sku, :version)"),
+        dict(sku=sku, version=product_version),
+    )
     session.execute(
         text("INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
              " VALUES (:ref, :sku, :qty, :eta)"),
-        dict(ref=ref, sku=sku, qty=qty, eta=eta)
+        dict(ref=ref, sku=sku, qty=qty, eta=eta),
     )
 
 
 def get_allocated_batch_ref(session, orderid, sku):
     [[orderlineid]] = session.execute(
         text("SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku"),
-        dict(orderid=orderid, sku=sku)
+        dict(orderid=orderid, sku=sku),
     )
     [[batchref]] = session.execute(
         text("SELECT b.reference FROM allocations JOIN batches AS b ON batch_id = b.id"
              " WHERE orderline_id=:orderlineid"),
-        dict(orderlineid=orderlineid)
+        dict(orderlineid=orderlineid),
     )
     return batchref
 
 
-def test_uow_can_retrive_at_batch_and_allocate_to_it(session_factory):
+def test_uow_can_retrieve_a_batch_and_allocate_to_it(session_factory):
     session = session_factory()
-
-    insert_batch(session, "batch1", "WOOD-WORKBENCH", 100, None)
+    insert_batch(session, "batch1", "HIPSTER-WORKBENCH", 100, None)
     session.commit()
 
     uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
-
     with uow:
-        batch = uow.batches.get(reference="batch1")
-        line = model.OrderLine("o1", "WOOD-WORKBENCH", 10)
-        batch.allocate(line)
+        product = uow.products.get(sku="HIPSTER-WORKBENCH")
+        line = model.OrderLine("o1", "HIPSTER-WORKBENCH", 10)
+        product.allocate(line)
         uow.commit()
 
-    batchref = get_allocated_batch_ref(session, "o1", "WOOD-WORKBENCH")
+    batchref = get_allocated_batch_ref(session, "o1", "HIPSTER-WORKBENCH")
     assert batchref == "batch1"
 
 
@@ -49,22 +51,20 @@ def test_rolls_back_uncommitted_work_by_default(session_factory):
         insert_batch(uow.session, "batch1", "MEDIUM-PLINTH", 100, None)
 
     new_session = session_factory()
-    rows = list(new_session.execute(text("SELECT * FROM 'batches'")))
-
+    rows = list(new_session.execute(text('SELECT * FROM "batches"')))
     assert rows == []
 
 
 def test_rolls_back_on_error(session_factory):
-    class TestException(Exception):
+    class MyException(Exception):
         pass
 
     uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
-    with pytest.raises(TestException):
+    with pytest.raises(MyException):
         with uow:
-            insert_batch(uow.session, "batch1", "MEDIUM-PLINTH", 100, None)
-            raise TestException()
+            insert_batch(uow.session, "batch1", "LARGE-FORK", 100, None)
+            raise MyException()
 
     new_session = session_factory()
-    rows = list(new_session.execute(text("SELECT * FROM 'batches'")))
-
+    rows = list(new_session.execute(text('SELECT * FROM "batches"')))
     assert rows == []
--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,25 +1,23 @@
 import pytest
-from service_layer import services
-from datetime import date, timedelta
 
-from adapters.repository import FakeRepository
-from service_layer import unit_of_work
-
-today = date.today()
-tomorrow = today + timedelta(days=1)
-later = tomorrow + timedelta(days=2)
+from adapters import repository
+from service_layer import unit_of_work, services
 
 
-class FakeSession:
-    committed = False
+class FakeRepository(repository.AbstractRepository):
+    def __init__(self, products):
+        self._products = set(products)
 
-    def commit(self):
-        self.committed = True
+    def add(self, product):
+        self._products.add(product)
+
+    def get(self, sku):
+        return next((p for p in self._products if p.sku == sku), None)
 
 
 class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
     def __init__(self):
-        self.batches = FakeRepository([])
+        self.products = FakeRepository([])
         self.committed = False
 
     def commit(self):
@@ -29,11 +27,18 @@ class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
         pass
 
 
-def test_add_batch_with_uow():
+def test_add_batch_for_new_product():
     uow = FakeUnitOfWork()
     services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, uow)
-    assert uow.batches.get("b1") is not None
+    assert uow.products.get("CRUNCHY-ARMCHAIR") is not None
     assert uow.committed
+
+
+def test_add_batch_for_existing_product():
+    uow = FakeUnitOfWork()
+    services.add_batch("b1", "GARISH-RUG", 100, None, uow)
+    services.add_batch("b2", "GARISH-RUG", 99, None, uow)
+    assert "b2" in [b.reference for b in uow.products.get("GARISH-RUG").batches]
 
 
 def test_allocate_returns_allocation():
@@ -43,34 +48,16 @@ def test_allocate_returns_allocation():
     assert result == "batch1"
 
 
-def test_returns_allocation():
+def test_allocate_errors_for_invalid_sku():
     uow = FakeUnitOfWork()
-    services.add_batch("b1", "SOME-LAMP", 100, None, uow)
-
-    result = services.allocate("o1", "SOME-LAMP", 10, uow)
-    assert result == "b1"
-
-
-# def test_add_batch():
-#     uow = FakeUnitOfWork()
-#
-#     services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, uow)
-#
-#     assert repo.get("b1") is not None
-#     assert session.committed
-
-
-def test_error_for_invalid_sku():
-    uow = FakeUnitOfWork()
-    services.add_batch("b1", "SOME-LAMP", 100, None, uow)
+    services.add_batch("b1", "AREALSKU", 100, None, uow)
 
     with pytest.raises(services.InvalidSku, match="Wrong sku NONEXISTENTSKU"):
         services.allocate("o1", "NONEXISTENTSKU", 10, uow)
 
 
-def test_commits():
+def test_allocate_commits():
     uow = FakeUnitOfWork()
-    services.add_batch('b1', 'OMINOUS-MIRROR', 100, None, uow)
-
-    services.allocate('o1', 'OMINOUS-MIRROR', 10, uow)
-    assert uow.committed is True
+    services.add_batch("b1", "OMINOUS-MIRROR", 100, None, uow)
+    services.allocate("o1", "OMINOUS-MIRROR", 10, uow)
+    assert uow.committed
