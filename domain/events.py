c569d8a47dfa675f2cf07a8a0e56eaf8032e0aea
Andrey Belskiy <anb@smena.space>
2024-04-01 08:43:18 +0500

    Add new events

--- domain/events.py
+++ domain/events.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import date
 
 
 class Event:
@@ -8,3 +9,18 @@ class Event:
 @dataclass
 class OutOfStock(Event):
     sku: str
+
+
+@dataclass
+class BatchCreated(Event):
+    ref: str
+    sku: str
+    qty: str
+    eta: date | None = None
+
+
+@dataclass
+class AllocationRequired(Event):
+    orderid: str
+    sku: str
+    qty: int
