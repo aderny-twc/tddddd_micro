2ec3fdb434ce5f31a122015d11d960a7d6278cb1
Andrey Belskiy <anb@smena.space>
2024-04-01 09:40:36 +0500

    Update tests

--- tests/unit/test_handlers.py
+++ tests/unit/test_handlers.py
@@ -1,7 +1,8 @@
 import pytest
 
 from adapters import repository
-from service_layer import unit_of_work, services
+from domain import events
+from service_layer import unit_of_work, messagebus, handlers
 
 
 class FakeRepository(repository.AbstractRepository):
@@ -33,35 +34,61 @@ class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
 
 def test_add_batch_for_new_product():
     uow = FakeUnitOfWork()
-    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, uow)
+
+    messagebus.handle(
+        events.BatchCreated("b1", "CRUNCHY-ARMCHAIR", 100, None), uow
+    )
+
     assert uow.products.get("CRUNCHY-ARMCHAIR") is not None
     assert uow.committed
 
 
 def test_add_batch_for_existing_product():
     uow = FakeUnitOfWork()
-    services.add_batch("b1", "GARISH-RUG", 100, None, uow)
-    services.add_batch("b2", "GARISH-RUG", 99, None, uow)
+
+    messagebus.handle(
+        events.BatchCreated("b1", "GARISH-RUG", 100, None), uow
+    )
+
+    messagebus.handle(
+        events.BatchCreated("b2", "GARISH-RUG", 99, None), uow
+    )
+
     assert "b2" in [b.reference for b in uow.products.get("GARISH-RUG").batches]
 
 
 def test_allocate_returns_allocation():
     uow = FakeUnitOfWork()
-    services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
-    result = services.allocate("o1", "COMPLICATED-LAMP", 10, uow)
-    assert result == "batch1"
+
+    messagebus.handle(
+        events.BatchCreated("batch1", "COMPLICATED-LAMP", 100, None), uow
+    )
+
+    result = messagebus.handle(
+        events.AllocationRequired("o1", "COMPLICATED-LAMP", 10), uow
+    )
+
+    assert result == ["batch1"]
 
 
 def test_allocate_errors_for_invalid_sku():
     uow = FakeUnitOfWork()
-    services.add_batch("b1", "AREALSKU", 100, None, uow)
+    messagebus.handle(
+        events.BatchCreated("b1", "AREALSKU", 100, None), uow
+    )
 
-    with pytest.raises(services.InvalidSku, match="Wrong sku NONEXISTENTSKU"):
-        services.allocate("o1", "NONEXISTENTSKU", 10, uow)
+    with pytest.raises(handlers.InvalidSku, match="Wrong sku NONEXISTENTSKU"):
+        messagebus.handle(
+            events.AllocationRequired("o1", "NONEXISTENTSKU", 10), uow
+        )
 
 
 def test_allocate_commits():
     uow = FakeUnitOfWork()
-    services.add_batch("b1", "OMINOUS-MIRROR", 100, None, uow)
-    services.allocate("o1", "OMINOUS-MIRROR", 10, uow)
+    messagebus.handle(
+        events.BatchCreated("b1", "OMINOUS-MIRROR", 100, None), uow
+    )
+    messagebus.handle(
+        events.AllocationRequired("o1", "OMINOUS-MIRROR", 10), uow
+    )
     assert uow.committed
