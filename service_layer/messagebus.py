68c7dcbe8bc4884cc6ca529353935c2985d18323
Andrey Belskiy <anb@smena.space>
2024-04-10 09:14:27 +0500

    Create class messagebus for dep injection

--- service_layer/messagebus.py
+++ service_layer/messagebus.py
@@ -73,3 +73,55 @@ def handle_command(
     except Exception:
         logger.exception(f"Exception handling command {command}")
         raise
+
+
+class MessageBus:
+    def __init__(
+            self,
+            uow: unit_of_work.AbstractUnitOfWork,
+            event_handlers: dict[type[events.Event], list[callable]],
+            command_handlers: dict[type[commands.Command], callable],
+    ):
+        self.uow = uow
+        self.event_handlers = event_handlers
+        self.command_handlers = command_handlers
+
+    def handle(self, message: commands.Command | events.Event):
+        self.queue = [message]
+        while self.queue:
+            message = self.queue.pop(0)
+            if isinstance(message, events.Event):
+                self.handle_event(message)
+            elif isinstance(message, commands.Command):
+                self.handle_command(message)
+            else:
+                raise Exception(f"{message} wa not in Event or Command")
+
+    def handle_event(
+            self,
+            event: events.Event,
+            queue: list,
+    ):
+        for handler in self.event_handlers[type(event)]:
+            try:
+                for attempt in Retrying(stop=stop_after_attempt(3), wait=wait_exponential()):
+                    with attempt:
+                        logger.debug(f"handling event {event} with handler {handler}")
+                        handler(event)
+                        queue.extend(self.uow.collect_new_events())
+            except RetryError as retry_failure:
+                logger.error(f"Failed to process event {retry_failure.last_attempt.attempt_number} times")
+                continue
+
+    def handle_command(
+            self,
+            command: commands.Command,
+    ):
+        logger.debug(f"handling command {command}")
+        try:
+            handler = self.command_handlers[type(command)]
+            result = handler(command)
+            self.queue.extend(self.uow.collect_new_events())
+        except Exception:
+            logger.exception(f"Exception handling command {command}")
+            raise
